c7ee8f2749dfcfb2be0a6afd6cb03bf118b1fb79
wanger <958019947@qq.com>
2021-05-25 23:23:03 +0800

    写着玩

--- app/src/main/java/com/step/lclib/work/page/StorageActivity.kt
+++ app/src/main/java/com/step/lclib/work/page/StorageActivity.kt
@@ -2,6 +2,9 @@ package com.step.lclib.work.page
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import com.step.lclib.R
 import com.step.lclib.work.StorageCase
@@ -10,6 +13,15 @@ class StorageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_storage)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+
+        window.decorView.requestFocus()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         findViewById<Button>(R.id.btn_storage).setOnClickListener {
             StorageCase.test(this)
