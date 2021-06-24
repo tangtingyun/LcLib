a9e8663bb192e449df3b35222a3d8cc034b374e3
wanger <958019947@qq.com>
2021-06-24 23:20:21 +0800

    写着玩22

---
+++ app/src/main/java/com/step/lclib/work/kotlin/TestKotlin.kt
@@ -0,0 +1,53 @@
+package com.step.lclib.work.kotlin
+
+/*
+*
+*  apply 函数可以看作一个配置函数
+*
+*
+* */
+class TestKotlin {
+
+
+
+
+    fun format() {
+        "%.2f".format(8.956756);
+
+
+        "8.98".toIntOrNull()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
