ad83603e0f7c54349e9188a2c4542778586fa4dd
wanger <958019947@qq.com>
2021-06-29 23:38:28 +0800

    写着玩22

--- app/src/main/java/com/step/lclib/work/flow/FlowViewModel.kt
+++ app/src/main/java/com/step/lclib/work/flow/FlowViewModel.kt
@@ -4,14 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.step.lclib.work.lclog
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import org.json.JSONObject
 import java.lang.NullPointerException
+
+// https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow/
+// https://juejin.cn/post/6937138168474894343
+// https://developer.android.google.cn/kotlin/coroutines/coroutines-best-practices?hl=zh-cn
+// https://reduxkotlin.org/introduction/getting-started
+// https://github.com/Tinder
+// https://github.com/reduxkotlin
 
 class FlowViewModel : ViewModel() {
 
@@ -37,6 +43,32 @@ class FlowViewModel : ViewModel() {
                 }
         }
 
-//        job.cancel()
+        job.cancel()
     }
+
+
+    suspend fun cbflow() {
+        callbackFlow<Int> {
+            offer(1)
+            send(2)
+            awaitClose(
+
+            )
+        }
+
+        var stateIn = flow<Int> {
+
+        }.stateIn(viewModelScope)
+
+
+        stateIn.collect {
+
+        }
+        MutableStateFlow(1)
+
+        MutableSharedFlow<Int>(2, 2, BufferOverflow.DROP_LATEST).tryEmit(1)
+
+    }
+
+
 }
