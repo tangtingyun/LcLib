ebb23ec270b08957c8c6c5df1d312ab6470842c1
wanger <958019947@qq.com>
2021-07-10 23:40:26 +0800

    写着玩89

---
+++ app/src/main/java/com/step/lclib/work/utils/Cookies.kt
@@ -0,0 +1,16 @@
+package com.step.lclib.work.utils
+
+import android.webkit.CookieManager
+
+// https://sourcegraph.com/github.com/react-native-cookies/cookies/-/blob/android/src/main/java/com/reactnativecommunity/cookies/CookieManagerModule.java?utm_source=share
+// https://sourcegraph.com/github.com/Gamez0/WebView-with-cookieManager/-/blob/app/src/main/java/com/praylist/simpleqr/MainActivity.java?utm_source=share
+class Cookies {
+
+    fun sync() {
+        CookieManager.getInstance().setCookie("", "")
+        CookieManager.getInstance().flush()
+    }
+}
+
+
+
