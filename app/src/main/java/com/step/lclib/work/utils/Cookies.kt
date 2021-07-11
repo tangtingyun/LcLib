e37a9aee3bd278afc6ad49fc2354ad0498e4a881
wanger <958019947@qq.com>
2021-07-11 15:42:47 +0800

    添加适配

--- app/src/main/java/com/step/lclib/work/utils/Cookies.kt
+++ app/src/main/java/com/step/lclib/work/utils/Cookies.kt
@@ -4,6 +4,14 @@ import android.webkit.CookieManager
 
 // https://sourcegraph.com/github.com/react-native-cookies/cookies/-/blob/android/src/main/java/com/reactnativecommunity/cookies/CookieManagerModule.java?utm_source=share
 // https://sourcegraph.com/github.com/Gamez0/WebView-with-cookieManager/-/blob/app/src/main/java/com/praylist/simpleqr/MainActivity.java?utm_source=share
+
+
+/*
+*   mdn 关于 cookies 的定义
+*
+*   https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Cookies
+*
+* */
 class Cookies {
 
     fun sync() {
