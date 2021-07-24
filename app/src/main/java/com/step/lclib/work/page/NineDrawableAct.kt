589a1ba9fa4f4047085a8c7b31d54dd4f8a2bdaf
wanger <958019947@qq.com>
2021-07-24 11:53:38 +0800

    调整

---
+++ app/src/main/java/com/step/lclib/work/page/NineDrawableAct.kt
@@ -0,0 +1,17 @@
+package com.step.lclib.work.page
+
+import android.graphics.drawable.RotateDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.step.lclib.R
+
+class NineDrawableAct : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nine_drawable)
+
+        val rotateDrawable = RotateDrawable()
+
+
+    }
+}
